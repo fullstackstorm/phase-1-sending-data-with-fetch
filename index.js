11030f15b534bf0c600ba61952362721321336c0
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-04-19 01:34:16 -0700

    Appends information to the DOM.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData(name, email) {
+  const newBody = {
+    name: name,
+    email: email,
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(newBody),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((data) => appendIdToDOM(data))
+    .catch((error) => appendErrorToDOM(error.message));
+}
+
+function appendIdToDOM(data) {
+  const newElement = document.createElement("p");
+  const newContent = document.createTextNode(data.id);
+  newElement.appendChild(newContent);
+  document.body.appendChild(newElement);
+}
+
+function appendErrorToDOM(error) {
+  const newElement = document.createElement("p");
+  const newContent = document.createTextNode(error);
+  newElement.appendChild(newContent);
+  document.body.appendChild(newElement);
+}
